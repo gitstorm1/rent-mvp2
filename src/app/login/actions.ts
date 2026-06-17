5e6d808312318d148cd78bfeebebd4772bdae390
gitstorm1 <56407595+gitstorm1@users.noreply.github.com>
2026-06-17 08:53:29 +0500

    Add basic login and signup

---
+++ src/app/login/actions.ts
@@ -0,0 +1,50 @@
+'use server'
+
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
+import { createClient } from '@/lib/server'
+
+const isDev = process.env.NODE_ENV === 'development';
+
+export async function login(formData: FormData) {
+    const supabase = await createClient()
+
+    const email = formData.get('email') as string
+    const password = formData.get('password') as string
+
+    const { error } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+    })
+
+    if (error) {
+        if (isDev) {
+            console.error(error.message);
+        }
+        return redirect('/login?message=Could not authenticate user')
+    }
+
+    revalidatePath('/', 'layout')
+    redirect('/')
+}
+
+export async function signup(formData: FormData) {
+    const supabase = await createClient()
+
+    const email = formData.get('email') as string
+    const password = formData.get('password') as string
+
+    const { error } = await supabase.auth.signUp({
+        email,
+        password,
+    })
+
+    if (error) {
+        if (isDev) {
+            console.error(error.message);
+        }
+        return redirect('/login?message=Could not sign up user')
+    }
+
+    redirect('/login?message=Check your email to verify your account before logging in.')
+}
