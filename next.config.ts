c579bd3671112c954db88ca1a112d00786ed62cc
gitstorm1 <56407595+gitstorm1@users.noreply.github.com>
2026-06-19 13:01:07 +0500

    Increase upload limit

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     /* config options here */
     serverExternalPackages: ["pdf-parse"],
+    experimental: {
+        serverActions: {
+            bodySizeLimit: "30mb",
+        },
+        proxyClientMaxBodySize: "30mb",
+    }
 };
 
 export default nextConfig;
